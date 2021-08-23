d880839b73aadfeaf8c685013da02b44440c9af1
Jong Hyun Choi <70011908+choich323@users.noreply.github.com>
2021-08-24 00:09:35 +0900

    Code Arrange
    Separate Player Move codes into 3 functions

--- 3D Quater View Game/Assets/Scripts/Player.cs
+++ 3D Quater View Game/Assets/Scripts/Player.cs
@@ -21,19 +21,30 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        GetInput();
+        Move();
+        Turn();
+    }
+
+    void GetInput()
+    {
         hAxis = Input.GetAxisRaw("Horizontal");
         vAxis = Input.GetAxisRaw("Vertical");
         wDown = Input.GetButton("Walk");
+    }
 
+    void Move()
+    {
         moveVec = new Vector3(hAxis, 0, vAxis).normalized;
 
         transform.position += moveVec * speed * (wDown ? 0.4f : 1f) * Time.deltaTime; // 삼항 연산자
 
         anim.SetBool("isRun", moveVec != Vector3.zero); // 벡터가 0인지 아닌지
         anim.SetBool("isWalk", wDown);
+    }
 
-
+    void Turn()
+    {
         transform.LookAt(transform.position + moveVec);
-
     }
 }
