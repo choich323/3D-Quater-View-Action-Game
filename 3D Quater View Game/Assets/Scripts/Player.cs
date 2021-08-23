b1a9660789c7af7f260bb4d81bcd808b9547d7b3
Jong Hyun Choi <70011908+choich323@users.noreply.github.com>
2021-08-23 23:39:56 +0900

    Player Animation

--- 3D Quater View Game/Assets/Scripts/Player.cs
+++ 3D Quater View Game/Assets/Scripts/Player.cs
@@ -7,13 +7,15 @@ public class Player : MonoBehaviour
     public float speed;
     float hAxis;
     float vAxis;
+    bool wDown;
 
     Vector3 moveVec;
+    Animator anim;
 
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
-        
+        anim = GetComponentInChildren<Animator>();
     }
 
     // Update is called once per frame
@@ -21,9 +23,13 @@ public class Player : MonoBehaviour
     {
         hAxis = Input.GetAxisRaw("Horizontal");
         vAxis = Input.GetAxisRaw("Vertical");
+        wDown = Input.GetButton("Walk");
 
         moveVec = new Vector3(hAxis, 0, vAxis).normalized;
 
-        transform.position += moveVec * speed * Time.deltaTime;
+        transform.position += moveVec * speed * (wDown ? 0.4f : 1f) * Time.deltaTime; // 삼항 연산자
+
+        anim.SetBool("isRun", moveVec != Vector3.zero); // 벡터가 0인지 아닌지
+        anim.SetBool("isWalk", wDown);
     }
 }
