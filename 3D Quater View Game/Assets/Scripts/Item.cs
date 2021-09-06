514d21153884db7928885311155c0a4210eec957
Jong Hyun Choi <70011908+choich323@users.noreply.github.com>
2021-09-06 15:43:50 +0900

    Item collision prevention

--- 3D Quater View Game/Assets/Scripts/Item.cs
+++ 3D Quater View Game/Assets/Scripts/Item.cs
@@ -8,8 +8,26 @@ public class Item : MonoBehaviour
     public Type type;
     public int value;
 
+    Rigidbody rigid;
+    SphereCollider sphereCollider;
+
+    void Awake()
+    {
+        rigid = GetComponent<Rigidbody>();
+        sphereCollider = GetComponent<SphereCollider>(); // 첫번째 콜라이더가 선택됨
+    }
+
     void Update()
     {
         transform.Rotate(Vector3.up * 30 * Time.deltaTime);    
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "Floor")
+        {
+            rigid.isKinematic = true;
+            sphereCollider.enabled = false;
+        }    
     }
 }
