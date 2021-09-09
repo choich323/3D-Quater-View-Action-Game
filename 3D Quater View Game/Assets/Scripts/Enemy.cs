f557bbccd0826f3d5cd691bba6565dd46eca015f
Jong Hyun Choi <70011908+choich323@users.noreply.github.com>
2021-09-09 17:02:19 +0900

    Player tracking AI(Monster Nav. Logic)

--- 3D Quater View Game/Assets/Scripts/Enemy.cs
+++ 3D Quater View Game/Assets/Scripts/Enemy.cs
@@ -1,21 +1,41 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Enemy : MonoBehaviour
 {
     public int maxHealth;
     public int curHealth;
+    public Transform target;
 
     Rigidbody rigid;
     BoxCollider boxCollider;
     Material mat;
+    NavMeshAgent nav;
 
     void Awake()
     {
         rigid = GetComponent<Rigidbody>();
         boxCollider = GetComponent<BoxCollider>();
-        mat = GetComponent<MeshRenderer>().material;
+        mat = GetComponentInChildren<MeshRenderer>().material;
+        nav = GetComponent<NavMeshAgent>();
+    }
+
+    void Update()
+    {
+        nav.SetDestination(target.position);
+    }
+
+    void FreezeVelocity()
+    {
+        rigid.velocity = Vector3.zero;
+        rigid.angularVelocity = Vector3.zero;
+    }
+
+    void FixedUpdate()
+    {
+        FreezeVelocity();
     }
 
     void OnTriggerEnter(Collider other)
